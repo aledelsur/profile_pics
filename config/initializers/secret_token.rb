927cc7a40ea793c48f44c414086618e265a54a52
Ale - aledelsur <alealvarez_00@hotmail.com>
2013-03-14 00:57:27 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProfilePics::Application.config.secret_token = '3ee99e95688ad2394d4ba3658eaeb7b2e7bb517f3e39e6aead6ed6aa43a6538401cacb0cb16c1cc98fc3a70ac62e4801444cf1c20f41cb5c0ae3be820d151f9a'
